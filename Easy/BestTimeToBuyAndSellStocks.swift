ba1e570495c364fada5b8882a39f094b727a370b
ArigatouGozaimaster <modelsine@gmail.com>
2024-04-26 22:13:21 +1000

    Added new worked solutions

---
+++ Easy/BestTimeToBuyAndSellStocks.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        var maxProfit = 0
+        var leftIndex = 0
+
+        for i in 1..<prices.count {
+            if prices[i] < prices[leftIndex] {
+                leftIndex = i
+            }
+            let currentProfit = prices[i] - prices[leftIndex]
+            if currentProfit > maxProfit {
+                maxProfit = currentProfit
+            }
+        }
+        return maxProfit
+    }
+}
